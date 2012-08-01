e39063e9cadeb0049dc19d74a824f773adeeb22a
Andy Boxer <andrew.boxer@anobii.com>
2012-08-01 01:02:05 +0100

    extract class path from environment and load configuration

--- server/TeeTreeMakeTee.php
+++ server/TeeTreeMakeTee.php
@@ -6,8 +6,13 @@
  * @license Released under version 3 of the GNU public license - pls see http://www.opensource.org/licenses/gpl-3.0.html
  *
  */
+if($classPath = getenv("TEETREE_CLASS_PATH"))
+{
+    require_once($classPath. "/TeeTreeConfiguration.php");
+    require_once(__DIR__ . "/../config/TeeTreeBootStrap.php");
+    $tee = new TeeTreeTee();
+    die();
+}
 
-require_once(__DIR__ . "/../config/TeeTreeBootStrap.php");
-$tee = new TeeTreeTee();
-die();
+die("TeeTree configuration path was not set for making tee!");
 ?>
