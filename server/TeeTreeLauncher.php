f78f6f037fe0ee3b5215cf046ab819f016713acb
Andy Boxer <andrew.boxer@anobii.com>
2012-08-01 01:02:47 +0100

    ensure correct configuration file loaded

--- server/TeeTreeLauncher.php
+++ server/TeeTreeLauncher.php
@@ -7,10 +7,11 @@
  *
  */
 
-require_once(__DIR__ . "/../config/TeeTreeBootStrap.php");
-
 if(isset($argv) && (count($argv) === 4))
 {
+    require_once($argv[2]. "/TeeTreeConfiguration.php");
+    require_once(__DIR__ . "/../config/TeeTreeBootStrap.php");
+
     if($argv[3] === 'start')
     {
         $controller = new TeeTreeController($argv[2], $argv[1]);
