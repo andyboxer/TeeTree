1f12f59c6fbafb51480bda76031278c086cea70b
Andy Boxer <andrew.boxer@anobii.com>
2012-08-02 16:16:25 +0100

    comments

--- bootstrap/TeeTreeBootStrap.php
+++ bootstrap/TeeTreeBootStrap.php
@@ -7,31 +7,32 @@
  *
  */
 
+/**
+ *
+ * This class provides an autloader for the TeeTree object classes require to run the TeeTree client and server
+ *
+ */
 class TeeTreeBootStrap
 {
-    public static $BASE_PATH;
-
-    public static function boot()
-    {
-        self::$BASE_PATH = realpath(__DIR__ . "/../");
+    // This stores the home directory for this TeeTree installation.
+    public static $TEETREE_HOME;
+    // This stores the current directory and loads the autoloader
+    public static function boot() {
+        self::$TEETREE_HOME = realpath(__DIR__ . "/../");
         spl_autoload_register('TeeTreeBootStrap::autoLoad');
     }
-
-    public static function autoLoad($class)
-    {
+    // The autoloader is configured with references to all the require TeeTree directories
+    public static function autoLoad($class){
         $loaderGlobs = array("shared/", "server/" , "client/", "exceptions/");
-        foreach($loaderGlobs as $glob)
-        {
-            $load_files = glob(self::$BASE_PATH. "/{$glob}{$class}.php");
-            foreach($load_files as $file)
-            {
+        foreach($loaderGlobs as $glob){
+            $load_files = glob(self::$TEETREE_HOME. "/{$glob}{$class}.php");
+            foreach($load_files as $file){
                 require_once($file);
                 return;
             }
         }
     }
 }
-
+// When this file in included or required in client code we call boot automatically in order to configure the loader.
 TeeTreeBootStrap::boot();
-
 ?>
